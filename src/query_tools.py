d122a911b37eb072949c66423a4a18755cd72485
SemereWubshet <semerewubshet@gmail.com>
2024-11-13 23:06:02 +0100

    llms working in queries

--- src/query_tools.py
+++ src/query_tools.py
@@ -1,13 +1,10 @@
 import ollama
-import openai
-import re
 from openai import OpenAI
 
 
 
-with open('../templates/query_llm.txt', 'w') as f:
+with open('../templates/query_llm.txt', 'r') as f:
     query_role = f.read()
-print(query_role)
 
 def openai_gen_soc_questions(content):
     client = OpenAI()
@@ -18,9 +15,7 @@ def openai_gen_soc_questions(content):
             {"role": "user", "content": content}
         ]
     )
-    questions = response.choices[0].message
-    questions = re.findall(r'\d+\.\s(.*?\?)', str(questions))
-    questions = '\n'.join(questions)
+    questions = response.choices[0].message.content
     return questions
 
 def ollama_gen_soc_questions(content):
@@ -30,11 +25,4 @@ def ollama_gen_soc_questions(content):
     # print(response["message"]["content"])
 
     questions = response["message"]["content"]
-    # questions = re.findall(r'\d+\.\s(.*?\?)', questions)
     return questions
-
-# x = "The basic mammalian body type is quadrupedal, with most mammals using four limbs for terrestrial locomotion; but in some, the limbs are adapted for life at sea, in the air, in trees or underground. The bipeds have adapted to move using only the two lower limbs, while the rear limbs of cetaceans and the sea cows are mere internal vestiges. Mammals range in size from the 30–40 millimetres (1.2–1.6 in) bumblebee bat to the 30 metres (98 ft) blue whale—possibly the largest animal to have ever lived. Maximum lifespan varies from two years for the shrew to 211 years for the bowhead whale. All modern mammals give birth to live young, except the five species of monotremes, which lay eggs. The most species-rich group is the viviparous placental mammals, so named for the temporary organ (placenta) used by offspring to draw nutrition from the mother during gestation."
-#
-# out = ollama_gen_soc_questions(x)
-# print(type(out))
-# print(out)
