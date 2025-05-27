64f44b70df413bff2da1f79f93c37bb8f72c6ce6
dhia <mediouni@eurecom.fr>
2025-05-27 17:04:33 +0200

    wip

--- src/tmp.py
+++ src/tmp.py
@@ -1,12 +1,13 @@
 from socratic_rl_peft import Gemma
 
 if __name__ == "__main__":
-    s = Gemma()
+    s = Gemma(device="cuda:0")
 
     chat = [
         {"role": "user", "content": "What is the answer to life the universe and everything?"},
         {"role": "model", "content": "Arrr, 'tis 42,"},
         {"role": "user", "content": "Where does this comes from?"}
     ]
-    s.query(chat)
+    a = s.query(chat)
+    print(a)
     pass
