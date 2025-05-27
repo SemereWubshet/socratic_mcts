7170857a2bbd1cfca3cfcf1794342aff8cd9d7e2
dhia <mediouni@eurecom.fr>
2025-05-27 16:46:15 +0200

    wip

---
+++ src/tmp.py
@@ -0,0 +1,11 @@
+from src.socratic_rl_peft import Gemma
+
+if __name__ == "__main__":
+    s = Gemma()
+
+    chat = [
+        {"role": "user", "content": "What is the answer to life the universe and everything?"},
+        {"role": "model", "content": "Arrr, 'tis 42,"},
+    ]
+    s.query(chat)
+    pass
