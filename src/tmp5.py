e36fe889dda184fa6e00e9f5f773114a51e02ad2
Giovanni Gatti Pinheiro <giovanni.gatti.pinheiro@gmail.com>
2025-06-04 16:12:39 +0200

    [test phi4] missing device

--- src/tmp5.py
+++ src/tmp5.py
@@ -5,3 +5,5 @@ if __name__ == "__main__":
     model = AutoModelForCausalLM.from_pretrained(
         "microsoft/Phi-4-mini-instruct", torch_dtype=torch.float16, trust_remote_code=True
     )
+    model.to(torch.device("cuda"))
+
