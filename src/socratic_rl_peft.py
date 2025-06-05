77d29bc5b949f5c3560e8c6e7495a48f612deee6
Giovanni Gatti Pinheiro <giovanni.gatti.pinheiro@gmail.com>
2025-06-05 06:22:13 +0200

    [test phi4] specifying target modules

--- src/socratic_rl_peft.py
+++ src/socratic_rl_peft.py
@@ -355,7 +355,7 @@ def policy_train(
         lora_alpha=32,
         lora_dropout=0.05,
         bias="none",
-        # target_modules=["q_proj", "o_proj", "k_proj", "v_proj", "gate_proj", "up_proj", "down_proj"],
+        target_modules=["q_proj", "o_proj", "k_proj", "v_proj", "gate_proj", "up_proj", "down_proj"],
         task_type="CAUSAL_LM",
     )
 
