3ba9de6474fb2f32b02e16faa75cce0125ad144d
Giovanni Gatti Pinheiro <giovanni.gatti.pinheiro@gmail.com>
2025-07-03 14:25:07 +0200

    bug fix

--- src/dataset_export.py
+++ src/dataset_export.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
     eval_dataset = EvaluationDataset.model_validate_json(evaluation_dataset_path.read_text())
 
     output = {"messages": []}
-    for e in filter(lambda _e: not _e.assessment, eval_dataset.evaluations):
+    for e in filter(lambda _e: _e.assessment, eval_dataset.evaluations):
         formatted = []
         for h in e.interaction.chat_history.root:
             role = "user" if h.role == "Student" else "assistant"
             formatted.append({"role": role, "content": h.content})
         output["messages"].append(formatted)
 
-    dataset = Dataset.from_list(output)
+    dataset = Dataset.from_dict(output)
     dataset.save_to_disk("../datasets/stf_examples")
