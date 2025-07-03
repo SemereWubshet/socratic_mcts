4feab05bbcfe9010dfb7bbedaa04c05c6dee2880
Giovanni Gatti Pinheiro <giovanni.gatti.pinheiro@gmail.com>
2025-07-03 14:15:58 +0200

    conversational dataset

--- src/dataset_export.py
+++ src/dataset_export.py
@@ -8,18 +8,13 @@ if __name__ == "__main__":
     evaluation_dataset_path = Path("../datasets/evaluation/eval_16_mistral-small3.1_24b.json")
     eval_dataset = EvaluationDataset.model_validate_json(evaluation_dataset_path.read_text())
 
-    output = []
+    output = {"messages": []}
     for e in filter(lambda _e: _e.assessment, eval_dataset.evaluations):
-        for i in range(1, len(e.interaction.chat_history.root), 2):
-            hist = e.interaction.chat_history.root[:i]
-            answer = e.interaction.chat_history.root[i]
-            formatted = []
-            for h in hist:
-                role = "user" if h.role == "Student" else "assistant"
-                formatted.append({"role": role, "content": h.content})
-            # TODO: format https://huggingface.co/docs/trl/en/sft_trainer#dataset-format-support
-            # Example: https://huggingface.co/datasets/philschmid/dolly-15k-oai-style/viewer/default/train?views%5B%5D=train&row=2
-            output.append({"prompt": formatted, "completion": {"role": "assistant", "content": answer.content}})
+        formatted = []
+        for h in e.interaction.chat_history.root:
+            role = "user" if h.role == "Student" else "assistant"
+            formatted.append({"role": role, "content": h.content})
+        output["messages"].append(formatted)
 
     dataset = Dataset.from_list(output)
     dataset.save_to_disk("../datasets/stf_examples")
