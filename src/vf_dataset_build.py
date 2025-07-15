c8d10548e31213edee2acbff49def74ad9d73498
Giovanni Gatti Pinheiro <giovanni.gatti.pinheiro@gmail.com>
2025-07-15 15:34:45 +0200

    use_bfloat16=False

--- src/vf_dataset_build.py
+++ src/vf_dataset_build.py
@@ -156,7 +156,7 @@ class ActionValueFn:
             pretrained_model_name_or_path=self._base_model,
             num_labels=1,
             torch_dtype=torch.float16,
-            # use_bfloat16=False,
+            use_bfloat16=False,
             problem_type="regression",
             device_map="cuda"
         )
