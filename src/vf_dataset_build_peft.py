798c3ad49b6c15f7c42f07c46ce9e3a1ae1268c3
Giovanni Gatti Pinheiro <giovanni.gatti.pinheiro@gmail.com>
2025-07-21 14:40:27 +0200

    pushing peft alternative - fixes

--- src/vf_dataset_build_peft.py
+++ src/vf_dataset_build_peft.py
@@ -150,18 +150,10 @@ class ActionValueFn:
         )
 
     def save(self, path: pathlib.Path) -> None:
-        self.model.merge_and_unload()
         self.model.save_pretrained(path)
         self.tokenizer.save_pretrained(path)
 
     def load(self, for_inference: bool = True) -> None:
-        peft_config = LoraConfig(
-            r=4,
-            lora_alpha=32,
-            task_type=TaskType.SEQ_CLS,
-            target_modules="all-linear"
-        )
-
         if pathlib.Path(self._base_model).exists() and pathlib.Path(self._base_model).is_dir():
             config = PeftConfig.from_pretrained(self._base_model)
             self.model = AutoModelForSequenceClassification.from_pretrained(config.base_model_name_or_path)
@@ -169,12 +161,17 @@ class ActionValueFn:
                 self.model, self._base_model, is_trainable=not for_inference, config=config
             )
         else:
-            self.model = ModernBertForSequenceClassification.from_pretrained(
+            self.model = ModernBertModel.from_pretrained(
                 pretrained_model_name_or_path="answerdotai/ModernBERT-large",
-                num_labels=1,
                 torch_dtype=torch.float32,
                 problem_type="regression",
                 device_map="cuda"
+            )
+            peft_config = LoraConfig(
+                r=4,
+                lora_alpha=32,
+                task_type=TaskType.SEQ_CLS,
+                target_modules="all-linear"
             )
             self.model = get_peft_model(self.model, peft_config)
 
